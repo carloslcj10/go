ecbbe2723f6665910fe86cc8fea46a5ce4f4d0df
carloslcj10 <carloslcj10@gmail.com>
2019-09-11 20:14:49 -0300

    adicionar package

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+	 "github.com/carloslcj10/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("Hello, world."))
 }
